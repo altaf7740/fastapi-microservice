9d1315da750007695af2a43591364b99f0db20f0
Md Altaf Hussain <48275876+altaf7740@users.noreply.github.com>
2025-09-28 13:01:35 +0530

    fix: settings.json

--- src/app/routes.py
+++ src/app/routes.py
@@ -1,20 +1,8 @@
-import sys
-
 from fastapi import APIRouter
-
-from core.config.logging import logger
 
 router = APIRouter()
 
 
-@router.get("/get-health")
+@router.get("/")
 async def health_check():
-    logger.info("this is logger object for info")
-    logger.debug("this is logger object for debug")
-    logger.error("this is logger object for error")
-    logger.critical("this is logger object for critical")
-    logger.warning("this is logger object for warning")
-    print("this is general print")
-    print("this is stdout print", file=sys.stdout)
-    print("this is std err print", file=sys.stderr)
     return {"message": "app is runnings"}
